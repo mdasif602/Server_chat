7ba3cce8d58b75bd10828f19b706ad60dda0ec78
MD ASIF HUSSAIN <mdasifhussain268@gmail.com>
2023-09-29 19:26:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,22 +4,41 @@ const io = require('socket.io')(process.env.PORT || 8080)
 
 const users = {};
 
+function getIST() {
+    const now = new Date();
+    const ISTOffset = 5.5 * 60; // IST is UTC+5.5
+    const ISTTime = new Date(now.getTime() + ISTOffset);
+    return ISTTime;
+}
+
+function formatIST(time) {
+    return time.toLocaleString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true });
+}
+
+
+
 io.on('connection', socket =>{
     // If any new user joins, let other users connected to the server know
-    socket.on('new-user-joined', name =>{
+    socket.on('new-user-joined', user =>{
         // console.log("New user", name)
-        users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name);
+        users[socket.id] = user;
+        const IST = getIST();
+        const formattedTime = formatIST(IST);
+        socket.broadcast.emit('user-joined', {name: user, time: formattedTime});
     });
 
     // If someone sends a message to all other people
     socket.on('send', message =>{
-        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
+        const IST = getIST();
+        const formattedTime = formatIST(IST);
+        socket.broadcast.emit('receive', {message: message, name: users[socket.id], time: formattedTime})
     });
     
     // If someone leaves the chat let other knows
     socket.on('disconnect', message =>{
-        socket.broadcast.emit('leave', users[socket.id]);
+        const IST = getIST();
+        const formattedTime = formatIST(IST);
+        socket.broadcast.emit('leave',{name: users[socket.id], time: formattedTime});
         delete users[socket.id];
     });
 
